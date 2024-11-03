783c1bacd9286e432e8a47ce66923cbbaf44e32a
xeomatrix369 <xeomatrix369@gmail.com>
2024-11-03 09:32:11 +0530

    i dont know commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({applyBaseStyles:false}), react()],
   site: 'https://xeomatrix369.github.io',
   base: '/xolio',
 });
